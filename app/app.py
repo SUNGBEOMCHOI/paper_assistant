ca3237b6e2d3f50224ae209bc74f5afb4ce7b024
SungBeom-Choi <choisb3631@gmail.com>
2024-02-22 03:16:43 +0900

    pdf viewer code(iframe)

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory, render_template
 
 from config import DevelopmentConfig
 
@@ -8,3 +8,12 @@ app.config.from_object(DevelopmentConfig)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/pdf/<filename>')
+def pdf_viewer(filename):
+    # Adjust the path according to your project structure
+    return send_from_directory('database/pdf', filename)
+
+@app.route('/view-pdf')
+def view_pdf():
+    return render_template('pdf_viewer.html')
